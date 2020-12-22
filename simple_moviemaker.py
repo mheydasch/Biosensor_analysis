201b8054b5965d71e404e586515d32843710b2aa
Max Heydasch <max@staff-208-63.eduroam.unibe.ch>
2020-12-22 15:15:05 +0100

    added microscope variants

--- simple_moviemaker.py
+++ simple_moviemaker.py
@@ -23,6 +23,8 @@ def parseArguments():
   parser = argparse.ArgumentParser(description='collect segmentation files into one directory')
   parser.add_argument('-d', '--dir', type=str, help='The directory where the knockdown folders are', required=True)
   parser.add_argument('-ch', '--chan', type=str, help='specify the channel you want to make a movie from', required =False)
+  parser.add_argument('-mic', '--mic', type=str, help='specify the microscope that generated the data. Either Jungfrau or Eiger', required =False)
+
   args = parser.parse_args()
   return(args)
   
@@ -33,7 +35,12 @@ def createFolder(directory):
     except OSError:
         print ('Error: Creating directory. ' + directory)
 def simple_moviemaker(path):
-    pattern=re.compile('.*(?P<Timepoint>_T[0-9]+)_(?P<Movie_ID>XY[0-9]+_.*).tif')
+    
+    if microscope=='Jungfrau':
+        
+        pattern=re.compile('.*(?P<Timepoint>_T[0-9]+)_(?P<Movie_ID>XY[0-9]+_.*).tif')
+    if microscope=='Eiger':
+        pattern=re.compile('^(?P<Classifier>.*)(?P<FOV>_[0-9])_.*(?P<Site>_s[0-9]+)_(?P<Timepoint>t[0-9]+).TIF')
     #pattern=re.compile('(?P<Movie_ID>.*)(?P<Timepoint>_t[0-9]+)')
     processed=[]
     
@@ -53,8 +60,12 @@ def simple_moviemaker(path):
                     current_Movie=[]
                     #extracts ID and timepoint
                     try:
-                        Movie_ID, Timepoint=re.search(pattern, item).group('Movie_ID', 'Timepoint')
-                        current_ID=Movie_ID
+                        if microscope=='Jungfrau':
+                            Movie_ID, Timepoint=re.search(pattern, item).group('Movie_ID', 'Timepoint')
+                            current_ID=Movie_ID
+                        if microscope=='Eiger':
+                            Classifier, FOV, Site, Timepoint=re.search(pattern, item).group('Classifier', 'FOV', 'Site', 'Timepoint')
+                            current_ID=Classifier+FOV+Site
                         #go to next interation of loop if ID is in processed
                         if current_ID in processed:
                             continue
@@ -90,12 +101,17 @@ def simple_moviemaker(path):
                     except (RuntimeError) as e:
                         print(e)
                         next        
+                else:
+                    print('Channel cannot be found')
     return processed
 
 if __name__ == '__main__':
     args=parseArguments()
     path=args.dir
     channel=args.chan
+    microscope=args.mic
+    if args.mic == None:
+        microscope='Jungfrau'
     simple_moviemaker(path)
 
     print(args)
